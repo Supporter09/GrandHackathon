1a4521ce20bb909de8ad2d899d3dae0d55b4cd9c
buianhduc <buianhduc.popodoo2@gmail.com>
2020-10-05 15:35:11 +0700

    add firebase sdk to node modules

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ67Ve4ozcjpLZYsE_zxDSL1JkCZrq2aY",
+    authDomain: "grandhackathon.firebaseapp.com",
+    databaseURL: "https://grandhackathon.firebaseio.com",
+    projectId: "grandhackathon",
+    storageBucket: "grandhackathon.appspot.com",
+    messagingSenderId: "887814491453",
+    appId: "1:887814491453:web:1fa02eaaa1757e0d1d8954",
+    measurementId: "G-27KZCG9X9T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const db = firebase.firestore();
+  db.settings({ timestampsInSnapshots: true});
+  var storageRef = firebase.storage().ref();
+//firebase thing-yy;
+
+
+
+//declare vars
 const express = require('express');
 const router = express.Router();
 const port = 3000;
 const app = express();
 const cons = require('consolidate');
 const bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+const multer = require('multer');
+const upload = multer();
+const firebase_timestamp = new firebase.firestore.Timestamp;
 
+//app settings
 app.engine('html', cons.swig)
 app.set('views','./views');
 app.set('view engine', 'html');
@@ -22,9 +45,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array()); 
 app.use(express.static('public'));
 
-
-
-
+//routing
 
 app.get('/',(req,res) => {
     res.render('index');
@@ -47,8 +68,18 @@ app.get('/sign_up', (req,res) => {
 
 app.post('/upload-question',(req,res) => {
     console.log(req.body);
+    const input_data = req.body;
+    var infos = {
+        Email: input_data.email,
+        Name : input_data.name,
+        Question: input_data.question,
+        owned : NULL,
+        timePosted : 
+    }
     res.send('your queries have been recieved');
 })
 
+
+//main program
 console.log('listen on port http://localhost:3000');
 app.listen(port);
