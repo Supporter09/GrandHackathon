a96fe61a73fe3a45bc6b49ee6777dc8abfd266eb
buianhduc <buianhduc.popodoo2@gmail.com>
2020-10-18 09:15:23 +0700

    like and single-post-1

--- public/js/updateLike.js
+++ public/js/updateLike.js
@@ -1,3 +1,21 @@
-$('.heart').click(() => {
-    
+$('.heart').click(async () => {
+    const id = new URLSearchParams(window.location.search).get('id');
+    console.log(id);
+    var likes = await db.collection('Posts').doc(`${id}`).get();
+    likes = likes.data().likes;
+	let likeNum = Number($('#likeNumber').html());
+	document.getElementById('likeNumber').innerHTML = `${likeNum + 1}`;
+    db.collection('Posts')
+	    .doc(`${id}`)
+	    .update({
+		    likes: likes+1
+	    })
+	    .then(() => {
+	    	console.log('likes added');
+	    })
+})
+$('.comment').click(() => {
+	$('html, body').animate({
+		scrollTop: $(".comment").offset().top
+	}, 500);
 })
